8ec9ba4693d0f5d1f85abb6134fe46a7650fde5e
Nikos Mouzakitis <spereneka@gmail.com>
2015-08-21 17:52:54 +0300

    Add enabled property.

--- JPSVolumeButtonHandler/JPSVolumeButtonHandler.m
+++ JPSVolumeButtonHandler/JPSVolumeButtonHandler.m
@@ -35,11 +35,13 @@ static CGFloat minVolume                    = 0.00001f;
         [self disableVolumeHUD];
         
         // Wait for the volume view to be ready before setting the volume to avoid showing the HUD
-        double delayInSeconds = 1.0f;
+        double delayInSeconds = 0.4f;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             [self setInitialVolume];
         });
+        
+        _enabled = YES;
     }
     return self;
 }
@@ -102,6 +104,16 @@ static CGFloat minVolume                    = 0.00001f;
     }
 }
 
+- (void)setEnabled:(BOOL)enabled {
+    if (enabled) {
+        [self disableVolumeHUD];
+    } else {
+        [self.volumeView removeFromSuperview];
+    }
+    
+    _enabled = enabled;
+}
+
 - (void)disableVolumeHUD {
     self.volumeView = [[MPVolumeView alloc] initWithFrame:CGRectMake(MAXFLOAT, MAXFLOAT, 0, 0)];
     [[[[UIApplication sharedApplication] windows] firstObject] addSubview:self.volumeView];
@@ -135,6 +147,9 @@ static CGFloat minVolume                    = 0.00001f;
 #pragma mark - KVO
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (!self.enabled)
+        return;
+    
     if (context == sessionContext) {
         CGFloat newVolume = [change[NSKeyValueChangeNewKey] floatValue];
         CGFloat oldVolume = [change[NSKeyValueChangeOldKey] floatValue];
@@ -149,7 +164,6 @@ static CGFloat minVolume                    = 0.00001f;
         } else {
             if (self.downBlock) self.downBlock();
         }
-        
         // Reset volume
         [self setSystemVolume:self.initialVolume];
     } else {
@@ -162,7 +176,7 @@ static CGFloat minVolume                    = 0.00001f;
 - (void)setSystemVolume:(CGFloat)volume {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    [[MPMusicPlayerController applicationMusicPlayer] setVolume:(float)volume];
+    [[MPMusicPlayerController applicationMusicPlayer] setVolume:volume];
 #pragma clang diagnostic pop
 }
 
